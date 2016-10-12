ae81fb8abf6b7b0e47770e2068610c6d40900681
Rojas <grojas@dwsoftware.mx>
2016-10-12 11:46:11 -0500

    11.- Angular 2 Basics: Se modifico index, se agregaron archivos .ts y dependencias

--- App/app.component.js
+++ App/app.component.js
@@ -9,19 +9,27 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
-;
 var AppComponent = (function () {
     function AppComponent() {
+        this.message = 'Hello!';
+        // user = { id: 25, name: 'Rojas', username: 'grojas' };
+        this.users = [
+            { id: 25, name: 'Rojas', username: 'grojas' },
+            { id: 26, name: 'Santos', username: 'Snt' },
+            { id: 27, name: 'Estrada', username: 'Etd' },
+            { id: 28, name: 'Mendez', username: 'Mdz' },
+            { id: 29, name: 'Cadete', username: 'Cdt' }
+        ];
     }
-    AppComponent = __decorate([
-        core_1.Component({
-            selector: 'my-app',
-            template: "\n  <div class=\"jumbotron\">\n    <h1>Welcome to Our app!</h1>\n  </div>\n  ",
-            styles: ["\n    .jumbotron{ box-shadow: 0 2px 0 rgba(0,0,0,0.2);}\n    "]
-        }), 
-        __metadata('design:paramtypes', [])
-    ], AppComponent);
     return AppComponent;
 }());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'my-app',
+        template: "\n    <header>\n        <nav class=\"navbar navbar-inverse\">\n            <div class=\"nav-header\">\n                <a href=\"/\" class=\"navbar-brand\">My angular 2 app!</a>\n            </div>\n        </nav>\n    </header>\n\n    <main>\n        <div class=\"jumbotron\">\n            <h1>Welcome to Our app!</h1>\n            <p>{{message}}</p>\n        </div>\n        <!--    <p>The user is {{user.name}} ({{user.username}}).</p> -->\n\n        <div *ngIf=\"users\">\n            <div *ngFor=\"let user of users\">\n                {{user.name}} ({{user.username}})\n            </div>\n        </div>\n    </main>\n    \n    <footer class=\"text-center\">Copy right &copy; 2016</footer>\n  ",
+        styles: ["\n    .jumbotron{ box-shadow: 0 2px 0 rgba(0,0,0,0.2);}\n    "]
+    }),
+    __metadata("design:paramtypes", [])
+], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
--- App/app.component.ts
+++ App/app.component.ts
@@ -1,17 +1,45 @@
-import {Component}  from "@angular/core";;
+import {Component}  from "@angular/core";
 
 @Component({
     selector: 'my-app',
     template:
     `
-  <div class="jumbotron">
-    <h1>Welcome to Our app!</h1>
-  </div>
+    <header>
+        <nav class="navbar navbar-inverse">
+            <div class="nav-header">
+                <a href="/" class="navbar-brand">My angular 2 app!</a>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="jumbotron">
+            <h1>Welcome to Our app!</h1>
+            <p>{{message}}</p>
+        </div>
+        <!--    <p>The user is {{user.name}} ({{user.username}}).</p> -->
+
+        <div *ngIf="users">
+            <div *ngFor="let user of users">
+                {{user.name}} ({{user.username}})
+            </div>
+        </div>
+    </main>
+    
+    <footer class="text-center">Copy right &copy; 2016</footer>
   `,
     styles: [`
     .jumbotron{ box-shadow: 0 2px 0 rgba(0,0,0,0.2);}
     `]
 })
 export class AppComponent {
-
+    message = 'Hello!';
+    // user = { id: 25, name: 'Rojas', username: 'grojas' };
+    users = [
+        { id: 25, name: 'Rojas', username: 'grojas' },
+        { id: 26, name: 'Santos', username: 'Snt' },
+        { id: 27, name: 'Estrada', username: 'Etd' },
+        { id: 28, name: 'Mendez', username: 'Mdz' },
+        { id: 29, name: 'Cadete', username: 'Cdt' }
+    ];
 }
